419e99452e54a3a7d21aceff6e78d82cfc525d80
Dudi <dudi.chen@gmail.com>
2019-08-21 05:41:07 +0300

    Few post merge adjustments

---
+++ GameBoardData/GameBoardData.cs
@@ -0,0 +1,122 @@
+﻿
+using System;
+using BullsAndCows.GameBoard;
+////using BullsAndCows.GameBoard.Colors;
+using BullsAndCows.GameProperties.Colors;
+////using GameProperties = BullsAndCows.GameProperties.Properties;
+
+namespace BullsAndCows.GameBoard
+{
+    using GameProperties = BullsAndCows.GameProperties.Properties;
+    public class GameBoardData
+    {
+        ////public const int k_MinimumNumberOfTries = 4;
+        ////public const int k_MaximumNumberOfTries = 10;
+
+        public const uint k_NumberOfPinsToGuess = 4;
+
+        ////private Turn[] m_Turns = new Turn[k_MaximumNumberOfTries];
+        private Turn[] m_Turns;
+        private Pin[] m_GoalSequence;
+        private int m_NumberOfPinColors;
+
+        public GameBoardData(ushort i_NumberOfGuesses)
+        {
+            m_Turns = new Turn[i_NumberOfGuesses];
+            this.GoalSequence = GeneratePinGoalSequence();
+            m_NumberOfPinColors = Enum.GetNames(typeof(eColors)).Length;
+        }
+
+        public Pin[] GoalSequence
+        {
+            get
+            {
+                return m_GoalSequence;
+            }
+            private set
+            {
+                m_GoalSequence = value;
+            }
+        }
+
+        private Pin[] GeneratePinGoalSequence()
+        {
+            Pin[] goalPinsSequence = new Pin[GameProperties.PinsSequenceLength];
+
+            
+            for (int i = 0; i < goalPinsSequence.Length; i++)
+            {
+                Random randomizer = new Random();
+                int colorNumber = randomizer.Next(m_NumberOfPinColors);
+                eColors color = (eColors)colorNumber;
+                if (i > 0)
+                {
+                    while (!IsUniquePinColor(goalPinsSequence, color, i - 1))
+                    {
+                        colorNumber = randomizer.Next(m_NumberOfPinColors);
+                        color = (eColors)colorNumber;
+                    }
+                }
+
+                goalPinsSequence[i] = new Pin(color);
+                ////m_GoalSequence[i].Color = color;
+            }
+
+            return goalPinsSequence;
+        }
+
+        private bool IsUniquePinColor(Pin[] i_GoalPinSequence, eColors i_Color, int i_MaxIterator)
+        {
+            bool result = true;
+            int iterator = 0;
+
+            //// IN ORDER NOT TO USE i_MaxIterator:
+            ////foreach (Pin currentPin in m_GoalSequence)
+            ////{
+            ////    if (!currentPin.Equals(null))
+            ////    {
+            ////        if (i_Color == currentPin.Color)
+            ////        {
+            ////            result = false;
+            ////            break;
+            ////        }
+            ////    }
+            ////    else
+            ////    {
+            ////        break;
+            ////    }
+            ////}
+
+
+            foreach (Pin currentPin in i_GoalPinSequence)
+            {
+                if (iterator < i_MaxIterator)
+                {
+                    if (i_Color == currentPin.Color)
+                    {
+                        result = false;
+                        break;
+                    }
+                    else
+                    {
+                        iterator++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public Turn this[int i_TurnIndex]
+        {
+            get
+            {
+                return m_Turns[i_TurnIndex];
+            }
+        }
+    }
+}
---
+++ GameBoardData/Turn.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel;
+////using BullsAndCows.GameBoard.Colors;
+using BullsAndCows.GameProperties.Colors;
+////using GameProperties = BullsAndCows.GameProperties.Properties;
+
+namespace BullsAndCows.GameBoard
+{
+    using GameProperties = BullsAndCows.GameProperties.Properties;
+    ////public enum eResultFlags
+    ////{
+    ////    None, Bull, Cow
+    ////}
+    ////namespace Colors
+    ////{
+    ////    public enum eColors
+    ////    {
+    ////        Green, Red, Blue, Yellow, Orange, Grey, Pink, Brown
+    ////    }
+    ////}
+
+    public class Turn
+    {
+
+        ////public struct Pin
+        ////{
+        ////    private eColors m_Color;
+
+        ////    public Pin(eColors i_Color)
+        ////    {
+        ////        m_Color = i_Color;
+        ////    }
+
+        ////    public eColors Color
+        ////    {
+        ////        get
+        ////        {
+        ////            return m_Color;
+        ////        }
+        ////    }
+        ////}
+
+        public struct Result
+        {
+            public ushort m_CorrectInPlacePins;
+            public ushort m_CorrectMisplacedPins;
+
+            public Result(ushort i_CorrectInPlacePins, ushort i_CorrectMisplacedPins)
+            {
+                m_CorrectInPlacePins = i_CorrectInPlacePins;
+                m_CorrectMisplacedPins = i_CorrectMisplacedPins;
+            }
+
+            public ushort CorrectInPlacePins
+            {
+                get
+                {
+                    return m_CorrectInPlacePins;
+                }
+                set
+                {
+                    m_CorrectInPlacePins = value;
+                }
+            }
+
+            public ushort CorrectMisplacedPins
+            {
+                get
+                {
+                    return m_CorrectMisplacedPins;
+                }
+                set
+                {
+                    m_CorrectMisplacedPins = value;
+                }
+            }
+        }
+
+        //private string m_guess;
+        private readonly Pin[] m_GuessedPins = new Pin[GameProperties.PinsSequenceLength];
+        private readonly Result m_Result;
+
+        public Turn(Pin[] i_GuessedPins, Result i_Result)
+        {
+            m_GuessedPins = i_GuessedPins;
+            m_Result = i_Result;
+        }
+
+        public Result Results
+        {
+            get
+            {
+                return m_Result;
+            }
+        }
+
+        public Pin[] Guess
+        {
+            get
+            {
+                return m_GuessedPins;
+            }
+        }
+    }
+}
