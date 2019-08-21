8349a152237c9bd23a72fa8bb652fdcd8e6cf7d4
Dudi <dudi.chen@gmail.com>
2019-08-21 17:31:40 +0300

    removed test references from Main()

--- C19_Ex02_1/Program.cs
+++ C19_Ex02_1/Program.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using BullsAndCows;
 
-// For Testing:
-using BullsAndCows.GameBoard;
-using BullsAndCows.GameProperties.Colors;
-using GameProperties = BullsAndCows.GameProperties.Properties;
-//
 
 namespace BullsAndCows
 {
